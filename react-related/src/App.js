752b9a19e779fed55e2a9aece051e2b62aefd291
Reno Lewis Gagnon <reno.lewisgagnon@gmail.com>
2020-12-04 12:34:50 -0500

    second update

--- react-related/src/App.js
+++ react-related/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          onClick={() => alert("Hello")}
+          startIcon={SaveIcon}
+          style={{
+            fontSize: 40,
+          }}
+          size="Large"
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
